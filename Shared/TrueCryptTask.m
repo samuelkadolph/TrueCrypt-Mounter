b075cba311a2af61a000c2264843dce5adfe34dd
Samuel Kadolph <samuel@kadolph.com>
2013-01-13 20:15:02 -0500

    Remove trailing whitespace

--- Shared/TrueCryptTask.m
+++ Shared/TrueCryptTask.m
@@ -15,7 +15,7 @@
   NSURL * url = [[NSWorkspace sharedWorkspace] URLForApplicationWithBundleIdentifier:@"org.TrueCryptFoundation.TrueCrypt"];
   NSBundle * bundle = [NSBundle bundleWithPath:[url path]];
   NSString * path = [bundle executablePath];
-  
+
   return [path autorelease];
 }
 + (NSTask *)runTrueCryptTask:(id)firstArgument, ...
@@ -23,24 +23,24 @@
   va_list vargs;
   va_start(vargs, firstArgument);
   NSMutableArray * args = [NSMutableArray array];
-  
+
   for (id argument = firstArgument; argument != nil; argument = va_arg(vargs, id))
     [args addObject:argument];
-  
+
   NSTask * task = [[[NSTask alloc] init] autorelease];
-  
+
   [task setLaunchPath:[self getTrueCryptPath]];
   [task setArguments:args];
-  
+
   NSPipe * output = [NSPipe pipe];
   NSPipe * error = [NSPipe pipe];
-  
+
   [task setStandardOutput:output];
   [task setStandardError:error];
-  
+
   [task launch];
   [task waitUntilExit];
-  
+
   return task;
 }
 
@@ -54,28 +54,28 @@
   NSString * name = [[path lastPathComponent] stringByDeletingPathExtension];
   NSString * mountPath = [NSString stringWithFormat:@"/Volumes/%@", name];
   NSTask * task = [self runTrueCryptTask:path, mountPath, nil];
-  
+
   if ([task terminationStatus] == 0)
   {
     return mountPath;
   }
-  
+
   exit(1);
 }
 + (NSString *)volumeMountPath:(NSString *)path
 {
   NSTask * task = [self runTrueCryptTask:@"--text", @"--list", path, nil];
-  
+
   if ([task terminationStatus] == 0)
   {
     NSFileHandle * output = [[task standardOutput] fileHandleForReading];
     NSData * data = [output readDataToEndOfFile];
-    NSString * result = [[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] autorelease];    
-    
+    NSString * result = [[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] autorelease];
+
     NSArray * parts = [result componentsMatchedByRegex:@"[^\\s\"']+|\"[^\"]*\"|'[^']*'"];
     NSCharacterSet * quotes = [NSCharacterSet characterSetWithCharactersInString:@"\"'"];
     NSString * mountPath = [[parts objectAtIndex:3] stringByTrimmingCharactersInSet:quotes];
-    
+
     return mountPath;
   }
   else
--- TrueCrypt Mounter/AppDelegate.m
+++ TrueCrypt Mounter/AppDelegate.m
@@ -15,45 +15,45 @@
 static void submitUnmounterService(NSString * path, NSString * mountPath)
 {
   launch_data_t job = launch_data_alloc(LAUNCH_DATA_DICTIONARY);
-  
+
   int pid = [[NSProcessInfo processInfo] processIdentifier];
   NSString * label = [NSString stringWithFormat:@"org.samuelkadolph.TrueCrypt-Mounter.%d", pid];
   const char * cLabel = [label cStringUsingEncoding:NSUTF8StringEncoding];
   const char * unmounter = [[[NSBundle mainBundle] pathForResource:@"unmounter" ofType:@""] UTF8String];
   const char * cPath = [path UTF8String];
   const char * cMountPath = [mountPath UTF8String];
-  
+
   launch_data_dict_insert(job, launch_data_new_string(cLabel), LAUNCH_JOBKEY_LABEL);
-  
+
   launch_data_t args = launch_data_alloc(LAUNCH_DATA_ARRAY);
   launch_data_array_set_index(args, launch_data_new_string(unmounter), 0);
   launch_data_array_set_index(args, launch_data_new_string(cLabel), 1);
   launch_data_array_set_index(args, launch_data_new_string(cPath), 2);
   launch_data_dict_insert(job, args, LAUNCH_JOBKEY_PROGRAMARGUMENTS);
-  
+
   launch_data_t pathstate = launch_data_alloc(LAUNCH_DATA_DICTIONARY);
   launch_data_dict_insert(pathstate, launch_data_new_bool(FALSE), cMountPath);
-  
+
   launch_data_t keepalive = launch_data_alloc(LAUNCH_DATA_DICTIONARY);
   launch_data_dict_insert(keepalive, pathstate, LAUNCH_JOBKEY_KEEPALIVE_PATHSTATE);
   launch_data_dict_insert(job, keepalive, LAUNCH_JOBKEY_KEEPALIVE);
-  
+
   launch_data_t message = launch_data_alloc(LAUNCH_DATA_DICTIONARY);
   launch_data_dict_insert(message, job, LAUNCH_KEY_SUBMITJOB);
   launch_data_t response = launch_msg(message);
-  
+
   if (!response || launch_data_get_errno(response) != ERR_SUCCESS)
   {
     NSAlert * alert = [[[NSAlert alloc] init] autorelease];
-    
+
     [alert setAlertStyle:NSWarningAlertStyle];
     [alert setMessageText:@"Will not dismount automatically"];
     [alert setInformativeText:@"There was an error submitting the automatic dismount service. "
                                "The disk will not be dismounted when you eject the volume."];
-    
+
     [alert runModal];
   }
-  
+
   launch_data_free(message);
   launch_data_free(response);
 }
@@ -70,29 +70,29 @@ static void submitUnmounterService(NSString * path, NSString * mountPath)
   if (![[NSWorkspace sharedWorkspace] URLForApplicationWithBundleIdentifier:@"org.TrueCryptFoundation.TrueCrypt"])
   {
     NSAlert * alert = [[[NSAlert alloc] init] autorelease];
-    
+
     [alert setAlertStyle:NSCriticalAlertStyle];
     [alert setMessageText:@"Cannot find TrueCrypt"];
     [alert setInformativeText:@"TrueCrypt could not be found.\nMake sure it is installed."];
-    
+
     [alert runModal];
     exit(2);
   }
-  
+
   if (!volumeFilename)
   {
     NSOpenPanel * dialog = [NSOpenPanel openPanel];
-    
+
     [dialog setCanChooseFiles:YES];
     [dialog setCanChooseDirectories:NO];
     [dialog setTitle:@"Open TrueCrypt Volume"];
-    
+
     if ([dialog runModal] == NSOKButton)
       volumeFilename = [[[dialog URLs] objectAtIndex:0] path];
     else
       exit(0);
   }
-  
+
   NSString * mountPath;
   if (![TrueCryptTask isVolumeMounted:volumeFilename])
   {
@@ -103,7 +103,7 @@ static void submitUnmounterService(NSString * path, NSString * mountPath)
   {
     mountPath = [TrueCryptTask volumeMountPath:volumeFilename];
   }
-  
+
   [[NSWorkspace sharedWorkspace] openFile:mountPath];
   exit(0);
 }
